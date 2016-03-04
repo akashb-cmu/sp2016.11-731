fe808415273ea375ee0f1db42608c2d5db7b430f
Akash Bharadwaj <akash.bharadwaj92@yahoo.com>
2016-03-03 20:41:00 -0500

    Added lowercasing

--- hw2/baseline.py
+++ hw2/baseline.py
@@ -1,11 +1,23 @@
-from utils import get_hypotheses
-
+from utils import *
+"""
+from gensim.models import Word2Vec as W2vec
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Merge
+from keras.layers.embeddings import Embedding
+from keras.layers import LSTM
+from keras.regularizers import l2
+from keras.optimizers import SGD
+"""
 HYP_FILE = "./data/train-test.hyp1-hyp2-ref"
 GOLD_FILE = "./data/train.gold"
 OUTPUT_FILE = "./output.pred"
-TRADEOFF_PARAM = 0.35# between 0 and 1
-
-[labeled_instances, unlabeled_instances] = get_hypotheses(HYP_FILE, GOLD_FILE)
+VEC_FILE = "./vecs/GoogleNews-vectors-negative300.bin"
+TRADEOFF_PARAM = 0.4# between 0 and 1 ---> closer to 0 means recall heavy, else, precision heavy
+"""
+TRAIN_SPLIT_RATIO = 0.85
+SENTENCE_EMBEDDING_LENGTH = 500
+"""
 
 def get_word_count_dict(sentence):
     word_list = sentence.split(" ")
@@ -80,11 +92,108 @@ def get_prediction(parallel_instance, tradeoff_param):
     else:
         return(0)
 
+"""
+print("Fetching google word vectors")
+
+pretrained_w2vec = W2vec.load_word2vec_format(VEC_FILE, binary=True)  # C binary format
+
+# Actually, try training your own word2vec model and use your vectors if they are absent from the google vecs
+[labeled_instances, unlabeled_instances, w2vec_model, word2index, index2word] = get_data_and_w2vec_dicts(HYP_FILE, GOLD_FILE, pretrained_w2vec)
+
+split_index = int(len(labeled_instances) * TRAIN_SPLIT_RATIO)
+"""
+
+#[labeled_instances, unlabeled_instances, w2vec_model, word2index, index2word] = get_data_and_w2vec_dicts(HYP_FILE, GOLD_FILE, pretrained_w2vec)
+[labeled_instances, unlabeled_instances, word2index, index2word] = get_data_and_w2vec_dicts(HYP_FILE, GOLD_FILE)
 
 with open(OUTPUT_FILE, 'w') as op_file:
+    #METEOR baseline method
     for instance in labeled_instances:
         parallel_instance = instance[0]
         op_file.write(str(get_prediction(parallel_instance, TRADEOFF_PARAM)) + '\n')
     for instance in unlabeled_instances:
         parallel_instance = instance
         op_file.write(str(get_prediction(parallel_instance, TRADEOFF_PARAM)) + '\n')
+    print("Finished writing output file with meteor method")
+
+
+"""
+    [train_ref_tensor, train_hyp1_tensor, train_hyp2_tensor, train_common_3way_labels], \
+    [test_ref_tensor, test_hyp1_tensor, test_hyp2_tensor, test_common_3way_labels], \
+    max_sentence_len = get_matrized_data(labeled_instances, word2index, TRAIN_SPLIT_RATIO)
+
+    print(labeled_instances[0][0][0])
+    print(labeled_instances[0][0][1])
+    print(labeled_instances[0][0][2])
+
+    print(train_hyp1_tensor[0])
+    print(train_hyp2_tensor[0])
+    print(train_ref_tensor[0])
+
+    print(labeled_instances[split_index][0][0])
+    print(labeled_instances[split_index][0][1])
+    print(labeled_instances[split_index][0][2])
+    print(test_hyp1_tensor[0])
+    print(test_hyp2_tensor[0])
+    print(test_ref_tensor[0])
+
+
+init_weights1 = np.random.rand(get_wvec_size(pretrained_w2vec), SENTENCE_EMBEDDING_LENGTH)
+init_weights2 = np.random.rand(SENTENCE_EMBEDDING_LENGTH, SENTENCE_EMBEDDING_LENGTH)
+init_weights3 = np.random.rand(SENTENCE_EMBEDDING_LENGTH,)
+
+# Constructing the ref_model
+print("Building ref_model")
+ref_model = Sequential()
+ref_model.add(Embedding(input_dim=len(word2index), output_dim=get_wvec_size(pretrained_w2vec), W_regularizer=l2(0.01), mask_zero=True, weights=[w2vec_model], dropout=0.2))
+# don't specify init since we are seeding with weights. Input len is used when you have a sequential network on top. It
+# is the max_length of a sentence in a matrix
+print("Initialized embedding matrix with pretrained word vecs")
+ref_model.add(LSTM(output_dim=SENTENCE_EMBEDDING_LENGTH, init='zero'))# weights=[init_weights1, init_weights2, init_weights3]))
+ref_model.add(Activation('tanh'))
+print("Added LSTM model")
+"""
+
+"""
+# Single representation check
+ref_model.add(Dense(output_dim=3, init='lecun_uniform', input_dim=SENTENCE_EMBEDDING_LENGTH))
+ref_model.add(Activation('softmax'))
+ref_model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+ref_model.fit(train_hyp1_tensor, train_common_3way_labels, batch_size=1024, nb_epoch=30, validation_split=0.1, show_accuracy=True, verbose=1)
+"""
+
+"""
+print("Building hyp1_model")
+hyp1_model = Sequential()
+hyp1_model.add(Embedding(input_dim=len(word2index), output_dim=get_wvec_size(pretrained_w2vec), W_regularizer=l2(0.01), mask_zero=True, weights=[w2vec_model], dropout=0.2))
+# don't specify init since we are seeding with weights. Input len is used when you have a sequential network on top. It
+# is the max_length of a sentence in a matrix
+print("Initialized embedding matrix with pretrained word vecs")
+hyp1_model.add(LSTM(output_dim=SENTENCE_EMBEDDING_LENGTH, init='zero')) #weights=[init_weights1, init_weights2, init_weights3]))
+hyp1_model.add(Activation('tanh'))
+print("Added LSTM model")
+
+print("Building hyp1_model")
+hyp2_model = Sequential()
+hyp2_model.add(Embedding(input_dim=len(word2index), output_dim=get_wvec_size(pretrained_w2vec), W_regularizer=l2(0.01), mask_zero=True, weights=[w2vec_model], dropout=0.2))
+# don't specify init since we are seeding with weights. Input len is used when you have a sequential network on top. It
+# is the max_length of a sentence in a matrix
+print("Initialized embedding matrix with pretrained word vecs")
+hyp2_model.add(LSTM(output_dim=SENTENCE_EMBEDDING_LENGTH, init='zero')) #weights=[init_weights1, init_weights2, init_weights3]))
+hyp2_model.add(Activation('tanh'))
+print("Added LSTM model")
+
+
+combined_model = Sequential()
+combined_model.add(Merge([ref_model, hyp1_model, hyp2_model], mode='concat'))
+#combined_model.add(Merge([ref_model, ref_model, ref_model], mode='concat'))
+
+print("Added merge layer")
+
+combined_model.add(Dense(output_dim=3, init='lecun_uniform', input_dim=3*SENTENCE_EMBEDDING_LENGTH))
+combined_model.add(Activation('softmax'))
+combined_model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01, momentum=0.01, decay=0.0, nesterov=False)) #optimizer='rmsprop')
+print("Added dense layer with softmax over classes")
+print(train_ref_tensor.shape, train_hyp1_tensor.shape, train_hyp2_tensor.shape)
+combined_model.fit([train_ref_tensor, train_hyp1_tensor, train_hyp2_tensor], train_common_3way_labels, batch_size=1024, nb_epoch=30, validation_split=0.1, show_accuracy=True, verbose=1)
+"""
--- hw2/utils.py
+++ hw2/utils.py
@@ -1,10 +1,33 @@
-def get_hypotheses(hyp_file_path, gold_file_path):
+import numpy as np
+
+def get_data_and_w2vec_dicts(hyp_file_path, gold_file_path): #, w2vec_model):
     corpus = []
     labels = []
+    #relevant_w2vec_model = []
+    word2index = {}
+    index2word = {}
+    max_word_index = 1
     with open(hyp_file_path, 'r') as hyp_file:
         for line in hyp_file:
             if len(line.strip(' \t\r\n')) > 0:
-                instance = (sub_str.strip(' \t\r\n') for sub_str in line.split('|||'))
+                instance = []
+                for sentence in line.split('|||'):
+                    sentence = sentence.strip(" \t\r\n").lower()
+                    instance.append(sentence)
+                    for sentence_index, word in enumerate(sentence.split(" ")):
+                        if word2index.get(word, None) is None:
+                            word2index[word] = max_word_index
+                            index2word[max_word_index] = word
+                            """
+                            try:
+                                #relevant_w2vec_model[word] = w2vec_model[word]
+                                relevant_w2vec_model.append(w2vec_model[word])
+                            except:
+                                #relevant_w2vec_model[word] = np.zeros(get_wvec_size(w2vec_model))
+                                relevant_w2vec_model.append(np.zeros(get_wvec_size(w2vec_model)))
+                            assert(len(relevant_w2vec_model) == max_word_index), "Index and embedding matrix length out of sync"
+                            """
+                            max_word_index += 1
                 corpus.append(instance)
     with open(gold_file_path, 'r') as gold_file:
         for line in gold_file:
@@ -12,4 +35,88 @@ def get_hypotheses(hyp_file_path, gold_file_path):
                 label = int(line.strip(' \t\r\n'))
                 labels.append(label)
     assert(len(labels) == len(corpus[:len(labels)])), "Len of corpus and labels doesn't match"
-    return([zip(corpus[:len(labels)], labels), corpus[len(labels):]])
+    return ([zip(corpus[:len(labels)], labels), corpus[len(labels):], word2index, index2word])
+    #return([zip(corpus[:len(labels)], labels), corpus[len(labels):], np.array(relevant_w2vec_model, dtype=np.float64), word2index, index2word])
+
+def get_wvec_size(w2vec_model):
+    return(len(w2vec_model['test']))
+
+def get_sentence_vector(sentence, max_len, word2index):
+    word_list = sentence.split(" ")
+    ret_vect = np.zeros(max_len, dtype=np.int32)
+    for w_index  in range(max_len):
+        if w_index < len(word_list):
+            word = word_list[w_index]
+            word_index = word2index.get(word, 0)
+            assert word_index != 0, "Word not found!"
+        else:
+            word_index = 0
+        ret_vect[w_index] = word_index
+    assert len(ret_vect) == max_len, "Returning mat of len < max_len"
+    return(ret_vect)
+
+def get_matrized_data(labeled_instances, word2index, train_split_ratio):
+    max_ref_len = max(len(t_instance[0][2].split(" ")) for t_instance in labeled_instances)
+    max_hyp1_len = max(len(t_instance[0][0].split(" ")) for t_instance in labeled_instances)
+    max_hyp2_len = max(len(t_instance[0][1].split(" ")) for t_instance in labeled_instances)
+
+    max_ref_len = max_hyp1_len = max_hyp2_len = max(max_ref_len, max_hyp1_len, max_hyp2_len)
+    print("Initializing tensors assuming index representation of word vectors")
+
+    ref_tensor = np.zeros((2*len(labeled_instances), max_ref_len), dtype=np.int32)
+    hyp1_tensor = np.zeros((2*len(labeled_instances), max_hyp1_len), dtype=np.int32)
+    hyp2_tensor = np.zeros((2*len(labeled_instances), max_hyp2_len), dtype=np.int32)
+
+    print("Initializing labels")
+
+    #hyp1_2way_labels = np.zeros(len(labeled_instances), dtype=np.int32)
+    #hyp2_2way_labels = np.zeros(len(labeled_instances), dtype=np.int32)
+    common_3way_labels = np.zeros((2*len(labeled_instances), 3), dtype=np.int32)
+
+    print("Building tensors")
+
+    for instance_index, instance in enumerate(labeled_instances):
+        [hyp1, hyp2, ref] = instance[0]
+        label = instance[1]
+        hyp1_sentence_vector = get_sentence_vector(hyp1, max_hyp1_len, word2index)
+        hyp2_sentence_vector = get_sentence_vector(hyp2, max_hyp2_len, word2index)
+        ref_sentence_vector = get_sentence_vector(ref, max_ref_len, word2index)
+
+        hyp1_tensor[2*instance_index] = hyp1_sentence_vector
+        hyp1_tensor[2 * instance_index + 1] = hyp2_sentence_vector
+        hyp2_tensor[instance_index] = hyp2_sentence_vector
+        hyp2_tensor[2 * instance_index + 1] = hyp1_sentence_vector
+        ref_tensor[2*instance_index] = ref_sentence_vector
+        ref_tensor[2*instance_index + 1] = ref_sentence_vector
+
+        # common_3way_labels[instance_index] = label
+
+        if label == -1:
+            common_3way_labels[2*instance_index] = [1,0,0]
+            common_3way_labels[2 * instance_index + 1] = [0, 0, 1]
+        elif label == 0:
+            common_3way_labels[2*instance_index] = [0,1,0]
+            common_3way_labels[2 * instance_index + 1] = [0, 1, 0]
+        else:
+            common_3way_labels[2*instance_index] = [0,0,1]
+            common_3way_labels[2*instance_index + 1] = [1, 0, 0]
+
+        """
+        if label == -1:
+            hyp1_2way_labels[instance_index] = 1
+            hyp1_2way_labels[instance_index] = 0
+        elif label == 1:
+            hyp1_2way_labels[instance_index] = 0
+            hyp2_2way_labels[instance_index] = 1
+        else:
+            hyp2_2way_labels[instance_index] = hyp1_2way_labels[instance_index] = 1
+        """
+    train_split_index = 2*int(len(labeled_instances) * train_split_ratio)
+    print("Building train split")
+    train_split = [elt[:train_split_index] for elt in
+                   [ref_tensor, hyp1_tensor, hyp2_tensor, common_3way_labels]]
+    print("Building test split")
+    test_split = [elt[train_split_index:] for elt in
+                   [ref_tensor, hyp1_tensor, hyp2_tensor, common_3way_labels]]
+    return([train_split, test_split, max_ref_len])
+
